d27d948ae9a301e6049ff285eed04a7e5a7bc928
Suhairayousuf <suhairaasif42@gmail.com>
2025-02-06 14:31:10 +0530

    event

--- lib/features/event/screens/event_detailes_widget.dart
+++ lib/features/event/screens/event_detailes_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:mayan/core/pallette/pallete.dart';
 
 import '../../../core/constants/variables.dart';
@@ -14,178 +15,223 @@ class _EventDetailsPageState extends State<EventDetailsPage> {
     return Scaffold(
       backgroundColor: Colors.white.withOpacity(0.97),
 
-      extendBody: true,
+      // extendBody: true,
       // extendBodyBehindAppBar: true,
       // backgroundColor: Colors.white,
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(60), // Adjusting app bar height
-        child: AppBar(
-          elevation: 0,
-          backgroundColor: primaryColor, // Setting the teal color for the app bar
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back, color: Colors.white),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
-          title: Text(
-            'Event details',
-            style: TextStyle(
-              fontFamily: 'Poppins',
-              fontWeight: FontWeight.w600,
-              fontSize: 18,
-              color: Colors.white,
-            ),
-          ),
-          actions: [
-            IconButton(
-              icon: Icon(Icons.calendar_today, color: Colors.white),
-              onPressed: () {
-                // Calendar action
-              },
-            ),
-          ],
-        ),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          // padding: const EdgeInsets.only(top: 90.0),
+      // appBar: PreferredSize(
+      //   preferredSize: Size.fromHeight(60), // Adjusting app bar height
+      //   child: AppBar(
+      //     elevation: 0,
+      //     backgroundColor: primaryColor, // Setting the teal color for the app bar
+      //     leading: IconButton(
+      //       icon: Icon(Icons.arrow_back, color: Colors.white),
+      //       onPressed: () {
+      //         Navigator.pop(context);
+      //       },
+      //     ),
+      //     title: Text(
+      //       'Event details',
+      //       style: TextStyle(
+      //         fontFamily: 'Poppins',
+      //         fontWeight: FontWeight.w600,
+      //         fontSize: 18,
+      //         color: Colors.white,
+      //       ),
+      //     ),
+      //     actions: [
+      //       IconButton(
+      //         icon: Icon(Icons.calendar_today, color: Colors.white),
+      //         onPressed: () {
+      //           // Calendar action
+      //         },
+      //       ),
+      //     ],
+      //   ),
+      // ),
+      body: Column(
+        // padding: const EdgeInsets.only(top: 90.0),
 
-        crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Event Title and Date
-            Container(
-              width: double.infinity,
-              height: 100,
-              color: primaryColor,
-              alignment: Alignment.centerLeft,
-              padding: EdgeInsets.symmetric(horizontal: 16),
-              child: Column(
-                children: [
-                  Text(
-                    'Event title here',
-                    style: TextStyle(
-                      fontFamily: 'Poppins',
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Colors.white,
-                    ),
-                  ),
-                  SizedBox(height: 8),
-                  Text(
-                    '12 August 2024',
-                    style: TextStyle(
-                      fontFamily: 'Poppins',
-                      fontSize: 14,
-                      color: Colors.grey[600],
-                    ),
-                  ),
-                ],
-              ),
-            ),
+      crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
 
-            SizedBox(height: 24),
+          // Event Title and Date
+          Container(
+            width: double.infinity,
+             height: 150,
+            color: primaryColor,
+            alignment: Alignment.centerLeft,
+             padding: EdgeInsets.symmetric(horizontal: 20),
+             child: Column(
+               crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                SizedBox(height: 20,),
+                Row(
+                  // mainAxisAlignment: MainAxisAlignment.start,
 
-            // Description
 
-            SizedBox(height: 8),
-            Card(
-              child: Container(
-                padding: const EdgeInsets.all(12.0),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: Column(
                   children: [
+                     Icon(Icons.arrow_back_ios_new, color: Colors.white,size: 20,),
+
+
+                    SizedBox(width: w*0.22,),
                     Text(
-                      'Description',
-                      style: TextStyle(
-                        fontFamily: 'Poppins',
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
-                        color: Colors.black,
-                      ),
-                    ),
-                    Text(
-                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-                      style: TextStyle(
-                        fontFamily: 'Poppins',
-                        fontSize: 14,
-                        color: Colors.grey[600],
+                      'Event details',
+                      style: GoogleFonts.poppins(
+                        // fontFamily: 'Poppins',
+                        // fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.white,
                       ),
                     ),
                   ],
                 ),
-              ),
-            ),
-            SizedBox(height: 24),
-
-            // Images Section
-            // _buildSectionHeader('Images', context),
-            SizedBox(height: 8),
-            _buildImageRow(),
-
-            SizedBox(height: 24),
-
-            // Videos Section
-            // _buildSectionHeader('Videos', context),
-            SizedBox(height: 8),
-            _buildVideoRow(),
-
-            SizedBox(height: 24),
-
-            // Recordings Section
-            // _buildSectionHeader('Recordings', context),
-            SizedBox(height: 8),
-            _buildRecordingRow(),
-
-            SizedBox(height: 24),
-
-            // Delete and Edit Buttons
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                TextButton.icon(
-                  onPressed: () {
-                    // Delete action
-                  },
-                  icon: Icon(Icons.delete, color: Colors.red),
-                  label: Text(
-                    'Delete',
-                    style: TextStyle(
-                      fontFamily: 'Poppins',
-                      fontSize: 14,
-                      color: Colors.red,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Event title here',
+                      style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
                     ),
-                  ),
+                    IconButton(
+                              icon: Icon(Icons.notification_important_outlined, color: Colors.white),
+                              onPressed: () {
+                                // Calendar action
+                              },
+                            ),
+                  ],
                 ),
-                ElevatedButton.icon(
-                  onPressed: () {
-                    // Edit action
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.teal,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 20, vertical: 12),
-                  ),
-                  icon: Icon(Icons.edit, color: Colors.white),
-                  label: Text(
-                    'Edit',
-                    style: TextStyle(
-                      fontFamily: 'Poppins',
-                      fontSize: 14,
-                      color: Colors.white,
-                    ),
+                // SizedBox(height: 8),
+                Text(
+                  '12 August 2024',
+                  style: GoogleFonts.poppins(
+                    // fontFamily: 'Poppins',
+                    fontSize: 14,
+                    color: Colors.white,
                   ),
                 ),
               ],
             ),
-          ],
-        ),
+          ),
+
+          SizedBox(height: 15),
+
+          // Description
+
+          // SizedBox(height: 8),
+         Padding(
+           padding: const EdgeInsets.only(left: 10.0,right: 10),
+           child: Flexible(
+             child: SingleChildScrollView(
+               physics: BouncingScrollPhysics(),
+               child: Column(
+                 children: [
+                   Card(
+                     child: Container(
+                       padding: const EdgeInsets.all(12.0),
+                       decoration: BoxDecoration(
+                         color: Colors.white,
+                         borderRadius: BorderRadius.circular(12),
+                       ),
+                       child: Column(
+                         children: [
+                           Text(
+                             'Description',
+                             style: TextStyle(
+                               fontFamily: 'Poppins',
+                               fontWeight: FontWeight.bold,
+                               fontSize: 16,
+                               color: Colors.black,
+                             ),
+                           ),
+                           Text(
+                             'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+                             style: TextStyle(
+                               fontFamily: 'Poppins',
+                               fontSize: 14,
+                               color: Colors.grey[600],
+                             ),
+                           ),
+                         ],
+                       ),
+                     ),
+                   ),
+                   SizedBox(height: 24),
+
+                   // Images Section
+                   // _buildSectionHeader('Images', context),
+                   SizedBox(height: 8),
+                   _buildImageRow(),
+
+                   SizedBox(height: 24),
+
+                   // Videos Section
+                   // _buildSectionHeader('Videos', context),
+                   SizedBox(height: 8),
+                   _buildVideoRow(),
+
+                   SizedBox(height: 24),
+
+                   // Recordings Section
+                   // _buildSectionHeader('Recordings', context),
+                   SizedBox(height: 8),
+                   _buildRecordingRow(),
+
+                   SizedBox(height: 24),
+
+                   // Delete and Edit Buttons
+                   Row(
+                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                     children: [
+                       TextButton.icon(
+                         onPressed: () {
+                           // Delete action
+                         },
+                         icon: Icon(Icons.delete, color: Colors.red),
+                         label: Text(
+                           'Delete',
+                           style: TextStyle(
+                             fontFamily: 'Poppins',
+                             fontSize: 14,
+                             color: Colors.red,
+                           ),
+                         ),
+                       ),
+                       ElevatedButton.icon(
+                         onPressed: () {
+                           // Edit action
+                         },
+                         style: ElevatedButton.styleFrom(
+                           backgroundColor: Colors.teal,
+                           shape: RoundedRectangleBorder(
+                             borderRadius: BorderRadius.circular(12),
+                           ),
+                           padding: const EdgeInsets.symmetric(
+                               horizontal: 20, vertical: 12),
+                         ),
+                         icon: Icon(Icons.edit, color: Colors.white),
+                         label: Text(
+                           'Edit',
+                           style: TextStyle(
+                             fontFamily: 'Poppins',
+                             fontSize: 14,
+                             color: Colors.white,
+                           ),
+                         ),
+                       ),
+                     ],
+                   ),
+                 ],
+               ),
+             ),
+           ),
+         )
+        ],
       ),
     );
   }
@@ -225,9 +271,8 @@ class _EventDetailsPageState extends State<EventDetailsPage> {
           children: [
             Text(
               'Images',
-              style: TextStyle(
-                fontFamily: 'Poppins',
-                fontWeight: FontWeight.bold,
+              style: GoogleFonts.poppins(
+                // fontWeight: FontWeight.bold,
                 fontSize: 16,
                 color: Colors.black,
               ),
@@ -266,9 +311,8 @@ class _EventDetailsPageState extends State<EventDetailsPage> {
               children: [
                 Text(
                   'Videos',
-                  style: TextStyle(
-                    fontFamily: 'Poppins',
-                    fontWeight: FontWeight.bold,
+                  style: GoogleFonts.poppins(
+                    // fontWeight: FontWeight.bold,
                     fontSize: 16,
                     color: Colors.black,
                   ),
@@ -308,10 +352,9 @@ class _EventDetailsPageState extends State<EventDetailsPage> {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Text(
-                  'Videos',
-                  style: TextStyle(
-                    fontFamily: 'Poppins',
-                    fontWeight: FontWeight.bold,
+                  'Recordings',
+                  style: GoogleFonts.poppins(
+                    // fontWeight: FontWeight.bold,
                     fontSize: 16,
                     color: Colors.black,
                   ),
@@ -341,8 +384,8 @@ class _EventDetailsPageState extends State<EventDetailsPage> {
 
   Widget _buildMediaCard(String assetPath) {
     return Container(
-      height: 80,
-      width: 80,
+      height: 60,
+      width: 120,
       decoration: BoxDecoration(
         color: Colors.grey[200],
         borderRadius: BorderRadius.circular(8),
